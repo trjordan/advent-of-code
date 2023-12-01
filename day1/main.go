eb6b960635f69f34706e831fa2638cfe76fe54da
TR Jordan <terral.jordan@gmail.com>
2023-12-01 13:45:07 -0500

    day1 part1 solution

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("heyo")
+	f, _ := os.Open("./input.txt")
+
+	scanner := bufio.NewScanner(f)
+
+	// regexps!!
+	firstNum, _ := regexp.Compile("[0-9]")
+	lastNum, _ := regexp.Compile(".*([0-9])")
+	s := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		//fmt.Println(line)
+		firstMatch := firstNum.FindString(line)
+		lastMatch := lastNum.FindStringSubmatch(line)
+		num := firstMatch + lastMatch[1]
+		//fmt.Println(num)
+		//fmt.Println("")
+		numStr, _ := strconv.Atoi(num)
+		s += numStr
+	}
+	fmt.Println(s)
 }
