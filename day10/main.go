a750811cd05a249bcaa7adab7a83f6d6ce10e561
TR Jordan <terral.jordan@gmail.com>
2023-12-11 20:53:14 -0500

    day10 part2

--- day10/main.go
+++ day10/main.go
@@ -8,25 +8,16 @@ import (
 )
 
 type tile struct {
-	pipe    rune
-	row     int
-	col     int
-	isStart bool
-	north   *tile
-	east    *tile
-	south   *tile
-	west    *tile
+	pipe     rune
+	row      int
+	col      int
+	isStart  bool
+	isOnPath bool
+	north    *tile
+	east     *tile
+	south    *tile
+	west     *tile
 }
-
-type direction int
-
-const (
-	unknown direction = iota
-	north
-	east
-	south
-	west
-)
 
 func pr(lbl string, t tile) {
 	if t.row == 1 && t.col == 1 {
@@ -34,7 +25,7 @@ func pr(lbl string, t tile) {
 	}
 }
 
-func parseGrid(fullGrid []string) *tile {
+func parseGrid(fullGrid []string) ([][]tile, *tile) {
 	var start *tile
 	tileGrid := make([][]tile, len(fullGrid))
 	for row, line := range fullGrid {
@@ -59,30 +50,31 @@ func parseGrid(fullGrid []string) *tile {
 			}
 		}
 	}
-	return start
+	return tileGrid, start
 }
 
 func neq(t1 tile, t2 tile) bool {
 	return !(t1.row == t2.row && t1.col == t2.col)
 }
 
-func nextTile(t tile, prev tile) (tile, tile) {
+func nextTile(t *tile, prev *tile) (*tile, *tile) {
 	orig := t.pipe
-	if t.north != nil && strings.ContainsRune("|7FS", (*t.north).pipe) && strings.ContainsRune("|JLS", orig) && neq(*t.north, prev) {
+	t.isOnPath = true
+	if t.north != nil && strings.ContainsRune("|7FS", t.north.pipe) && strings.ContainsRune("|JLS", orig) && neq(*t.north, *prev) {
 		fmt.Println("going north", t.row, t.col)
-		return *t.north, t
+		return t.north, t
 	}
-	if t.east != nil && strings.ContainsRune("-7JS", t.east.pipe) && strings.ContainsRune("-FLS", orig) && neq(*t.east, prev) {
+	if t.east != nil && strings.ContainsRune("-7JS", t.east.pipe) && strings.ContainsRune("-FLS", orig) && neq(*t.east, *prev) {
 		fmt.Println("going east", t.row, t.col)
-		return *t.east, t
+		return t.east, t
 	}
-	if t.south != nil && strings.ContainsRune("|JLS", (*t.south).pipe) && strings.ContainsRune("|7FS", orig) && neq(*t.south, prev) {
+	if t.south != nil && strings.ContainsRune("|JLS", t.south.pipe) && strings.ContainsRune("|7FS", orig) && neq(*t.south, *prev) {
 		fmt.Println("going south", t.row, t.col)
-		return *t.south, t
+		return t.south, t
 	}
-	if t.west != nil && strings.ContainsRune("-FLS", (*t.west).pipe) && strings.ContainsRune("-7JS", orig) && neq(*t.west, prev) {
+	if t.west != nil && strings.ContainsRune("-FLS", t.west.pipe) && strings.ContainsRune("-7JS", orig) && neq(*t.west, *prev) {
 		fmt.Println("going west", t.row, t.col)
-		return *t.west, t
+		return t.west, t
 	}
 	// oh no!!
 	panic("no valid tile found")
@@ -98,9 +90,9 @@ func main() {
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-	start := *parseGrid(lines)
+	tileGrid, start := parseGrid(lines)
 
-	// manually start its up!
+	// manually start its up! Collect the coords of pipes
 	prev := start
 	next, prev := nextTile(start, start)
 	distance := 1
@@ -109,5 +101,39 @@ func main() {
 		distance += 1
 		fmt.Println(next.pipe, next.row, next.col)
 	}
-	fmt.Println("Loop finished with distance", distance)
+	fmt.Println("grid", tileGrid)
+	fmt.Println("distance", distance)
+
+	// Cross-cut lines
+	// - empty spots after odd numbers of crossings are inside
+	// - empty sports after even numbers of crossings are inside
+	totalInside := 0
+	for row := 0; row < len(tileGrid); row++ {
+		tileLine := tileGrid[row]
+		fmt.Println(tileLine)
+		isInside := false
+		lastTraverseEntry := ' '
+		for col := 0; col < len(tileGrid[row]); col++ {
+			t := tileGrid[row][col]
+			if t.isOnPath {
+				if t.pipe == 'F' || t.pipe == 'L' {
+					// We're about to traverse a pipe, capture how we got in
+					lastTraverseEntry = t.pipe
+				} else if (lastTraverseEntry == 'L' && t.pipe == '7') || (lastTraverseEntry == 'F' && t.pipe == 'J') {
+					// We passed through a traverse that flipped us
+					isInside = isInside != true
+				} else if t.pipe == 'J' || t.pipe == '7' {
+					// We passed through a traverse that didn't flip us
+					lastTraverseEntry = ' '
+				} else if t.pipe == '|' {
+					// Simple flip
+					isInside = isInside != true
+				}
+			} else if isInside {
+				// Count it
+				totalInside += 1
+			}
+		}
+	}
+	fmt.Println("total inside", totalInside)
 }
